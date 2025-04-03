547533e38fe158a22ac622a924a60a8b4c495d00
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2025-04-03 15:59:15 +0300

    created singleton

--- src/Singleton/Program/Main.java
+++ src/Singleton/Program/Main.java
@@ -1,3 +1,4 @@
+package Singleton.Program;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/Singleton_Registry/Program/Main.java
@@ -0,0 +1,7 @@
+package Singleton_Registry.Program;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
