3401a84fd003c264d73bb444141f42dc1b224504
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2025-04-05 16:44:27 +0300

    solved SingletonRegistry

---
+++ src/SingletonRegistry/Classes/Preparat.java
@@ -0,0 +1,34 @@
+package SingletonRegistry.Classes;
+
+public class Preparat {
+    private String denumire;
+    private int timpAlocat;
+    private int gradeGatire;
+
+    public Preparat(String denumire, int timpAlocat, int gradeGatire) {
+        this.denumire = denumire;
+        this.timpAlocat = timpAlocat;
+        this.gradeGatire = gradeGatire;
+    }
+
+    public String getDenumire() {
+        return denumire;
+    }
+
+    public int getTimpAlocat() {
+        return timpAlocat;
+    }
+
+    public int getGradeGatire() {
+        return gradeGatire;
+    }
+
+    @Override
+    public String toString() {
+        return "Preparat{" +
+                "denumire='" + denumire + '\'' +
+                ", timpAlocat=" + timpAlocat +
+                ", gradeGatire=" + gradeGatire +
+                '}';
+    }
+}
