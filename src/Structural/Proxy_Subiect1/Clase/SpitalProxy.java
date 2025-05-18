307be725e00b7124c43867f4c2ccea9de3d37b30
Tofan Alexandrina <tofanalexandrina22@stud.ase.ro>
2025-05-18 22:45:15 +0300

    solved Proxy

--- src/Structural/Proxy_Subiect1/Clase/SpitalProxy.java
+++ src/Structural/Proxy_Subiect1/Clase/SpitalProxy.java
@@ -1,24 +1,36 @@
 package Structural.Proxy_Subiect1.Clase;
 
-public class SpitalProxy implements ISpital{
-    private Vizitator vizitator;
-    private boolean echipat;
+import java.util.ArrayList;
+import java.util.List;
 
-    public SpitalProxy(Vizitator vizitator) {
-        this.vizitator = vizitator;
-        this.echipat=false;
+public class SpitalProxy implements ISpital{
+    private ISpital spital;
+    private List<Vizitator>salaAsteptare;
+
+    public SpitalProxy(ISpital spital) {
+        this.spital = spital;
+        this.salaAsteptare=new ArrayList<>();
     }
 
     @Override
-    public void permiteAcces() {
-        if(vizitator.getAreScrisoareAcceptare()){
-            vizitator.permiteAcces();
-            if(!echipat){
-                echipat=true;
-                System.out.println(vizitator.getNume()+" a fost echipat cu haine de protectie.");
+    public void adaugaVizitator(Vizitator v) {
+        if (v.getAreScrisoareAcceptare()) {
+            salaAsteptare.add(v);
+            System.out.println(v.getNume() + " a fost adaugat in sala de asteptare.");
+
+            if (salaAsteptare.size() >= 5) {
+                System.out.println("Echipare vizitatori.");
+                for (Vizitator vizitator : salaAsteptare) {
+                    spital.permiteAcces(vizitator);
+                }
+                salaAsteptare.clear();
+                System.out.println("Acces permis pentru grup.");
             }
-        }else{
-            System.out.println("Nu este permis accesul vizitatorului "+ vizitator.getNume()+", nu are scrisoare de acceptare.");
         }
+    }
+
+    @Override
+    public void permiteAcces(Vizitator v) {
+        adaugaVizitator(v);
     }
 }
